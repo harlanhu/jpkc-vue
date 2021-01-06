1a78b90c3e8814eaaa207578fd5178e67cfc2b89
keyboardWithDream <isharlan.hu@gmail.com>
2021-01-06 22:04:23 +0800

    first commit

---
+++ vue.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  configureWebpack: {
+    resolve: {
+      alias: {
+        'assets': '@/assets',
+        'common': '@/common',
+        'components': '@/components',
+        'network': '@/network',
+        'views': '@/views'
+      }
+    }
+  },
+  devServer: {
+    host: 'localhost',
+    port: 80
+  },
+  lintOnSave: false
+}
