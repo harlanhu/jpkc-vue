d36028c42d00b1b9a06b6452a52e01863381a1c0
keyboardWithDream <isharlan.hu@gmail.com>
2021-01-07 16:52:40 +0800

    update

--- src/components/content/main/TopAside/AccountInfo.vue
+++ src/components/content/main/TopAside/AccountInfo.vue
@@ -1,12 +1,14 @@
 <template>
   <div id="account-info">
     <div id="logout">
+      <div class="title">免费学习来自名校名师的精品课程</div>
       <div class="avatar">
         <avatar/>
       </div>
       <div class="login-btn">
         <el-button class="login-btn" size="small" type="success" round>登录/注册</el-button>
       </div>
+      <div class="foot-info">网易和高等教育出版社出品</div>
     </div>
   </div>
 </template>
@@ -22,8 +24,52 @@ export default {
 </script>
 
 <style scoped>
+#account-info {
+  position: relative;
+  box-sizing: border-box;
+  display: flex;
+  align-items: center;
+  width: 200px;
+  height: 360px;
+}
+
+#account-info div {
+  width: 168px;
+  text-align: center;
+}
+
+.title {
+  position: absolute;
+  top: 24px;
+  left: 14px;
+  width: 160px;
+  line-height: 20px;
+  color: black;
+  text-align: center;
+  font-size: 15px;
+}
+
+.avatar {
+  position: relative;
+  text-align: center;
+  margin-left: 18px;
+}
+
 .login-btn {
   width: 168px;
   height: 32px;
+  position: relative;
+  margin-left: 8px;
+  margin-top: 10px;
+}
+
+.foot-info {
+  text-align: center;
+  font-size: 12px;
+  color: rgb(153, 153, 153);
+  position: absolute;
+  bottom: 20px;
+  font-weight: 400;
+  left: 16px;
 }
 </style>
