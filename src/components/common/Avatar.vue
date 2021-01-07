e27b419caeaedf9abdc40748e2865cfaf6726e63
keyboardWithDream <isharlan.hu@gmail.com>
2021-01-07 15:53:25 +0800

    update

--- src/components/common/Avatar.vue
+++ src/components/common/Avatar.vue
@@ -14,7 +14,7 @@ export default {
     },
     src: {
       type: String,
-      default: "https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+      default: "https://web-applications.oss-cn-chengdu.aliyuncs.com/jpck/user/default/avatar/user-default-avatar.png?Expires=1610008054&OSSAccessKeyId=TMP.3Ki7DbN7u74dGfs4gdiDR3ms3HtiS6USMB89gahX9rkbmfa4kL57jLcNgUJYgSm19qPDa8f5BkGtUJHqg2HM2AaDvHkmJr&Signature=CF1r0a767zmXP2jXwFtbYTOrJNY%3D&versionId=CAEQPRiBgIDjy_zuthciIDQ1OTQ4NzcxNmI2YzQzZGJiMmUyMjgwMDgyNDk5OGNk&response-content-type=application%2Foctet-stream"
     }
   }
 }
