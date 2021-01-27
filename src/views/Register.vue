08b9a0964143939e92e143335cce9f3fdc33aa92
keyboardWithDream <isharlan.hu@gmail.com>
2021-01-27 16:22:30 +0800

    feat(register): 注册页面短信发送更新

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -6,37 +6,60 @@
         <el-step title="验证手机号码"></el-step>
         <el-step title="完成注册"></el-step>
       </el-steps>
-      <el-alert v-show="errorMessage !== ''" :title="errorMessage" @close="clearErrorMessage" type="error" center show-icon/>
+      <el-alert v-show="errorMessage !== ''" :title="errorMessage" @close="clearErrorMessage" type="error" center
+                show-icon/>
       <div class="form-content">
-        <el-form :model="registerForm" status-icon :rules="rules" ref="registerForm" label-width="100px" class="registerForm">
-          <el-form-item v-show="active === 0" label-width="0px" prop="email">
-            <el-input type="text"
-                v-model="registerForm.email"
-                      autocomplete="off"
-                      placeholder="请输入邮箱地址"
-                      clearable/>
-          </el-form-item>
-          <el-form-item v-show="active === 1" label-width="0px" prop="phone">
-            <el-input type="text"
-                      v-model="registerForm.phone"
-                      autocomplete="off"
-                      placeholder="请输入手机号"
-                      clearable/>
-          </el-form-item>
-          <el-form-item label-width="0px" prop="password">
-            <el-input type="password"
-                      v-model="registerForm.password"
-                      autocomplete="off"
-                      minlength="8"
-                      maxlength="16"
-                      placeholder="请输入8-16位密码"
-                      show-password/>
-          </el-form-item>
-          <el-checkbox v-model="registerForm.isAllowedClause" v-show="active === 0">同意《xxx条款》</el-checkbox>
+        <el-form :model="registerForm" status-icon :rules="rules" ref="registerForm" label-width="100px"
+                 class="registerForm">
+          <div v-if="active === 0" class="email">
+            <el-form-item label-width="0px" prop="email">
+              <el-input type="text"
+                        v-model="registerForm.email"
+                        autocomplete="off"
+                        placeholder="请输入邮箱地址"
+                        clearable/>
+            </el-form-item>
+            <el-form-item label-width="0px" prop="password">
+              <el-input type="password"
+                        v-model="registerForm.password"
+                        autocomplete="off"
+                        minlength="8"
+                        maxlength="16"
+                        placeholder="请输入8-16位密码"
+                        show-password/>
+            </el-form-item>
+            <el-checkbox v-model="registerForm.isAllowedClause" v-show="active === 0">同意《xxx条款》</el-checkbox>
+          </div>
+          <div v-if="active === 1">
+            <div class="tip">为了确保帐号安全，您的手机号码将作为该帐号的安全手机。安全手机可用于找回密码、修改密保等操作。</div>
+            <el-form-item label-width="0px" prop="phone">
+              <el-input type="text"
+                        v-model="registerForm.phone"
+                        autocomplete="off"
+                        placeholder="请输入手机号码"
+                        maxlength="11"
+                        clearable/>
+            </el-form-item>
+            <el-collapse-transition>
+              <div v-show="smsBtnActive">
+                <div class="transition-box">
+                  <el-form-item label-width="0px" prop="smsVerifyCode">
+                    <el-input type="text"
+                              v-model="registerForm.smsVerifyCode"
+                              autocomplete="off"
+                              placeholder="请输入验证码"
+                              maxlength="4"
+                              clearable/>
+                  </el-form-item>
+                  <el-button @click="getSmsVerifyCode">{{smsBtnContent}}</el-button>
+                </div>
+              </div>
+            </el-collapse-transition>
+          </div>
         </el-form>
       </div>
       <div class="next-button">
-        <el-button @click="next">下一步</el-button>
+        <el-button @click="next">{{btnContent}}</el-button>
       </div>
     </div>
   </div>
@@ -53,7 +76,7 @@ export default {
         } else {
           this.$api.user.isExistUser(value)
               .then(res => {
-                if (res.status === 400){
+                if (res.status === 400) {
                   callback(new Error(res.message))
                 } else {
                   callback()
@@ -64,14 +87,17 @@ export default {
     }
 
     let phoneValidator = (rule, value, callback) => {
-      if (!/^[a-zA-Z][a-zA-Z0-9_]{3,15}$/.test(value)) {
+      if (!/^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/.test(value)) {
         callback(new Error("请填写正确的手机号!"))
-      }else {
+      } else {
         this.$api.user.isExistUser(value)
             .then(res => {
               if (res.status === 400) {
+                this.smsVerifyCode = ''
+                this.smsBtnActive = false
                 callback(new Error(res.message))
               } else {
+                this.smsBtnActive = true
                 callback()
               }
             })
@@ -81,9 +107,14 @@ export default {
     return {
       active: 0,
       errorMessage: "",
+      smsBtnActive: false,
+      isFirstGetCode: true,
+      sendSmsTime: 60,
       registerForm: {
         email: '',
         password: '',
+        phone: '',
+        smsVerifyCode: '',
         isAllowedClause: false
       },
       rules: {
@@ -96,6 +127,10 @@ export default {
         ],
         phone: [
           {validator: phoneValidator, trigger: 'blur'}
+        ],
+        smsVerifyCode: [
+          {required: true, message: '验证码不能为空', trigger: 'blur'},
+          {pattern: "^[0-9]{4}$", message: "请输入正确的验证码", trigger: 'blur'}
         ]
       }
     }
@@ -106,17 +141,58 @@ export default {
     },
     next() {
       this.$refs['registerForm'].validate((valid) => {
-        if (valid) {
-          if (this.registerForm.isAllowedClause) {
-            this.errorMessage = ''
-            this.active ++
+        if (this.active === 0) {
+          if (valid) {
+            if (this.registerForm.isAllowedClause) {
+              this.errorMessage = ''
+              this.active++
+            } else {
+              this.errorMessage = "请勾选同意条款"
+            }
           } else {
-            this.errorMessage = "请勾选同意条款"
+            this.errorMessage = "请填写正确的信息"
           }
-        }else {
-          this.errorMessage = "请填写正确的信息"
+        } else if (this.active === 1) {
+          if (valid) {
+
+          }
         }
       })
+    },
+    getSmsVerifyCode() {
+      this.isFirstGetCode = false
+      this.$api.smsVerifyCode.getSmsVerifyCode(this.registerForm.phone)
+          .then(res => {
+            console.log(res.message)
+            this.sendSmsVerifyTimeOn()
+          })
+    },
+    sendSmsVerifyTimeOn() {
+      return setInterval(() => {
+        console.log(this.sendSmsTime)
+        if (this.sendSmsTime > 0) {
+          this.sendSmsTime--
+        }else {
+          clearInterval(this.sendSmsVerifyTimeOn())
+        }
+      }, 1000)
+    }
+  },
+  computed: {
+    btnContent() {
+      switch (this.active) {
+        case 0:
+          return "下一步"
+        case 1:
+          return "立即验证"
+      }
+    },
+    smsBtnContent() {
+      if (this.isFirstGetCode) {
+        return "获取验证码"
+      }else {
+        return "重新发送(" + this.sendSmsTime + "秒)"
+      }
     }
   }
 }
@@ -149,5 +225,15 @@ export default {
   position: relative;
   margin: 0 auto;
   text-align: center;
+}
+
+.tip {
+  color: #333;
+  font-size: 14px;
+  background-color: #f8f8f8;
+  padding: 5px 12px;
+  border-radius: 4px;
+  line-height: 24px;
+  margin-bottom: 40px;
 }
 </style>
