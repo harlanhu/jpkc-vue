ee9e97a25ae1937a2f5d3ebea4cddb405c4f8c68
keyboardWithDream <isharlan.hu@gmail.com>
2021-02-05 14:19:02 +0800

    feat(advertising): 广告栏展示

--- src/components/common/StripCard.vue
+++ src/components/common/StripCard.vue
@@ -1,14 +1,22 @@
 <template>
   <div id="strip-card">
     <el-card :body-style="{ padding: '0px' }" shadow="hover">
-      <img src="https://shadow.elemecdn.com/app/element/hamburger.9cf7b091-55e9-11e9-a976-7f4d0b07eef6.png" class="image" alt="加载失败">
+      <img :src="data.resourcePath" class="image" alt="加载失败">
     </el-card>
   </div>
 </template>
 
 <script>
 export default {
-  name: "StripCard"
+  name: "StripCard",
+  props: {
+    data: {
+      type: Object,
+      default() {
+        return {}
+      }
+    }
+  }
 }
 </script>
 
--- src/components/content/main/home/advertising/Advertising.vue
+++ src/components/content/main/home/advertising/Advertising.vue
@@ -1,6 +1,8 @@
 <template>
  <div id="advertising">
-   <strip-card/>
+   <div class="content" v-for="data in dataList">
+     <strip-card :data="data"/>
+   </div>
  </div>
 </template>
 
@@ -10,10 +12,35 @@ export default {
   name: "Advertising",
   components: {
     StripCard
+  },
+  data() {
+    return {
+      dataList: []
+    }
+  },
+  methods: {
+    getAdvertising() {
+      this.$api.webResource.getWebResourceByLayoutName("home-advertising")
+      .then(res => {
+        this.dataList = res.data
+      })
+    }
+  },
+  created() {
+    this.getAdvertising()
   }
 }
 </script>
 
 <style scoped>
+#advertising {
+  display: flex;
+}
+
+.content {
+  float: left;
+  flex: 1;
+}
+
 
 </style>
