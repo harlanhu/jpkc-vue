30fd2f66cb317b1ce7361464027aade3b24a88da
keyboardWithDream <isharlan.hu@gmail.com>
2021-02-12 19:29:10 +0800

    feat(CommonCard): 公共卡片更新

---
+++ src/components/common/CommonCard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="common-card">
+    <el-card :body-style="{padding: 0}">
+
+    </el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CommonCard"
+}
+</script>
+
+<style scoped>
+
+</style>
