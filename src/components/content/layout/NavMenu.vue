b80511b2cf9d74ff80a18e3f418431b71ffd1426
keyboardWithDream <isharlan.hu@gmail.com>
2021-03-24 18:01:33 +0800

    update

--- src/components/content/layout/NavMenu.vue
+++ src/components/content/layout/NavMenu.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="nav-menu">
-
+    <el-menu class="el-menu-vertical-demo" :collapse="false" default-active="1" @open="handleOpen" @close="handleClose">
+      <template #title>
+        <i class="el-icon-document"/>
+        <span>布局列表</span>
+      </template>
+      <el-submenu index="1">
+        <el-menu-item-group>
+          <el-menu-item index="1-1">主页编辑推荐</el-menu-item>
+          <el-menu-item index="1-2">展示页编辑推荐</el-menu-item>
+        </el-menu-item-group>
+      </el-submenu>
+    </el-menu>
   </div>
 </template>
 
 <script>
 export default {
-name: "NavMenu"
+  name: "NavMenu",
+  methods: {
+    handleOpen(key, keyPath) {
+      console.log(key, keyPath)
+    },
+    handleClose(key, keyPath) {
+      console.log(key, keyPath)
+    }
+  }
 }
 </script>
 
--- src/views/LayoutManage.vue
+++ src/views/LayoutManage.vue
@@ -1,12 +1,16 @@
 <template>
-  <h1>布局信息</h1>
-
+  <div class="layout-manage">
+    <h1>布局信息</h1>
+    <nav-menu/>
+  </div>
 </template>
 
 <script>
 
+import NavMenu from "@/components/content/layout/NavMenu";
 export default {
   name: "LayoutManage",
+  components: {NavMenu},
   data() {
     return {
       layoutList: []
