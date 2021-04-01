3469a08267644205d9b0eb4e5c801da8e738edd2
keyboardWithDream <isharlan.hu@gmail.com>
2021-04-01 14:38:04 +0800

    update

--- src/components/content/main/home/banner/TopAside/CourseType.vue
+++ src/components/content/main/home/banner/TopAside/CourseType.vue
@@ -1,14 +1,50 @@
 <template>
   <div id="course-type">
+    <div class="tag" v-for="category in categoryList">
+      <el-tag type="info">{{category.categoryName}}</el-tag>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "CourseType"
+  name: "CourseType",
+  data() {
+    return {
+      categoryList: []
+    }
+  },
+  methods: {
+    getAllCategory() {
+      this.$api.category.getAllCategory()
+      .then(res => {
+        console.log(res.data)
+        this.categoryList = res.data
+      })
+    }
+  },
+  created() {
+    this.getAllCategory()
+  }
 }
 </script>
 
 <style scoped>
+#course-type {
+  display: flex;
+  display: -webkit-flex;
+  justify-content: space-between;
+  flex-direction: row;
+  flex-wrap: wrap;
+}
 
+.tag {
+  margin: 10px auto;
+  border-radius: 20px;
+  padding: 3px;
+}
+
+.el-tag {
+  border-radius: 20px;
+}
 </style>
